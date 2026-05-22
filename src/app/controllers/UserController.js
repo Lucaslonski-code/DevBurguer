69790435b97cab1d23e1953a66cd3750e6db2ca9
Lucaslonski-code <lucaslonski890@gmail.com>
2026-05-22 14:21:31 -0300

    new modifications, try/catch

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,25 +8,57 @@
 
 import { v4 } from 'uuid';
 import User from '../models/User.js';
+import * as Yup from 'yup';
+import bcrypt from 'bcrypt';
 
 class UserController {
   async store(request, response) {
-    try { const { name, email, password_hash, admin } = request.body;
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+      admin: Yup.boolean().required(),
+    });
 
-    const user = await User.create({ id: v4(), name, email, password_hash, admin });
+    try {
+      schema.validateSync(request.body, { abortEarly: false, strict: true });
+    } catch (error) {
+      return response.status(400).json({ error: error.errors });
+    }
 
-    return response.status(201).json({ message: 'User created', 
+    try {
+      const { name, email, password, admin } = request.body;
+
+      const existingUser = await User.findOne({
+        where: { email },
+      });
+
+      if (existingUser) {
+        return response.status(400).json({ message: 'Email already in use' });
+      }
+
+      const password_hash = await bcrypt.hash(password, 10);
+
+      const user = await User.create({
+        id: v4(),
+        name,
+        email,
+        password_hash,
+        admin,
+      });
+
+      return response.status(201).json({
+        message: 'User created',
         id: user.id,
         name: user.name,
         email: user.email,
-        admin: user.admin
-     });
-     
-  } catch (error) {
-    console.error('Erro ao criar usuário:', error);
-    return response.status(400).json({ error: error.message });
+        admin: user.admin,
+      });
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error);
+      return response.status(400).json({ error: error.message });
+    }
   }
-}}
-
+}
 
 export default new UserController();
