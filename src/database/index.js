58ceff3b106b6daae883121ef9a20bc0e1cd151e
Lucaslonski-code <lucaslonski890@gmail.com>
2026-06-01 20:08:57 -0300

    new database, mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import mongoose from 'mongoose';
 import Users from '../app/models/User.js';
 import databaseConfig from '../config/database.cjs';
 import Product from '../app/models/Product.js';
@@ -9,11 +10,16 @@ const models = [Users, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
     models.map((model) => model.init(this.connection)).map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo(){
+    this.mongooseConection = mongoose.connect('mongodb://localhost:27017/dev-burguer-api-mongo')
   }
 }
 
